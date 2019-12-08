aca414a977e58ca1a67219e580e41b889099e6f0
Remus Selea <remus.selea@gmail.com>
2019-12-08 13:20:25 +0200

    ScentDB Initial commit. Health actuator endpoint is up.

---
+++ src/main/java/com/github/remusselea/scentdb/ScentdbApplication.java
@@ -0,0 +1,13 @@
+package com.github.remusselea.scentdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScentdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScentdbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/remusselea/scentdb/ScentdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.remusselea.scentdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScentdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
