0f7cac8886188cc0e7e97dcc12eb343f90aab7a9
remus-selea <58348422+remus-selea@users.noreply.github.com>
2020-03-29 16:40:19 +0300

    Note 19 sonar analysis integration (#4)
    * Add sonarqube plugin
    
    * Add gradlew script in .travis.yml
    
    * Add mockito, change logging root level to info and add active profile for tests

--- src/test/java/com/github/remusselea/scentdb/ScentdbApplicationTests.java
+++ src/test/java/com/github/remusselea/scentdb/ScentdbApplicationTests.java
@@ -2,8 +2,10 @@ package com.github.remusselea.scentdb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({ "h2" })
 class ScentdbApplicationTests {
 
     @Test
