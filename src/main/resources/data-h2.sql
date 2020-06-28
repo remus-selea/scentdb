26c61a57ef4f67876498dcc1f8bc249912199243
Remus Selea <remus.selea@gmail.com>
2020-06-28 21:51:46 +0300

    Make scentdb work on docker or without docker - Bind volume and update application.properties - Update data-h2.sql notes

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,11 +1,18 @@
-
-
-
-INSERT INTO notes (note_id, note_name, img_path) VALUES (1, 'Leather', 'path1');
-INSERT INTO notes (note_id, note_name, img_path) VALUES (2, 'Opoponax', 'path2');
-INSERT INTO notes (note_id, note_name, img_path) VALUES (3, 'Oregano', 'path3');
-INSERT INTO notes (note_id, note_name, img_path) VALUES (4, 'Pepper', 'path4');
-INSERT INTO notes (note_id, note_name, img_path) VALUES (5, 'Labdanum', 'path5');
-INSERT INTO notes (note_id, note_name, img_path) VALUES (6, 'Bergamot', 'path6');
+-- NOTES
+INSERT INTO notes (note_id, note_name, img_path) VALUES (1, 'Leather', 'http://localhost:8321/scentdb/v1/images/notes/leather.jpg');
+INSERT INTO notes (note_id, note_name, img_path) VALUES (2, 'Opoponax', 'http://localhost:8321/scentdb/v1/images/notes/opoponax.jpg');
+INSERT INTO notes (note_id, note_name, img_path) VALUES (3, 'Oregano', 'http://localhost:8321/scentdb/v1/images/notes/oregano.jpg');
+INSERT INTO notes (note_id, note_name, img_path) VALUES (4, 'Pepper', 'http://localhost:8321/scentdb/v1/images/notes/pepper.jpg');
+INSERT INTO notes (note_id, note_name, img_path) VALUES (5, 'Labdanum', 'http://localhost:8321/scentdb/v1/images/notes/labdanum.jpg');
+INSERT INTO notes (note_id, note_name, img_path) VALUES (6, 'Bergamot', 'http://localhost:8321/scentdb/v1/images/notes/bergamot.jpg');
+INSERT INTO notes (note_id, note_name, img_path) VALUES (7, 'Amber', 'http://localhost:8321/scentdb/v1/images/notes/amber.jpg');
+INSERT INTO notes (note_id, note_name, img_path) VALUES (8, 'Incense', 'http://localhost:8321/scentdb/v1/images/notes/incense.jpg');
+INSERT INTO notes (note_id, note_name, img_path) VALUES (9, 'Patchouli', 'http://localhost:8321/scentdb/v1/images/notes/patchouli.jpg');
+INSERT INTO notes (note_id, note_name, img_path) VALUES (10, 'Agarwood', 'http://localhost:8321/scentdb/v1/images/notes/agarwood.jpg');
+INSERT INTO notes (note_id, note_name, img_path) VALUES (11, 'Sandalwood', 'http://localhost:8321/scentdb/v1/images/notes/sandalwood.jpg');
+INSERT INTO notes (note_id, note_name, img_path) VALUES (12, 'Rose', 'http://localhost:8321/scentdb/v1/images/notes/rose.jpg');
+INSERT INTO notes (note_id, note_name, img_path) VALUES (13, 'Nutmeg', 'http://localhost:8321/scentdb/v1/images/notes/nutmeg.jpg');
+INSERT INTO notes (note_id, note_name, img_path) VALUES (14, 'Tuberose', 'http://localhost:8321/scentdb/v1/images/notes/tuberose.jpg');
+INSERT INTO notes (note_id, note_name, img_path) VALUES (15, 'Peach', 'http://localhost:8321/scentdb/v1/images/notes/peach.jpg');
 
 
