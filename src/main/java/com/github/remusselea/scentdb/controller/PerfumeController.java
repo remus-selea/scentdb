93ce9178285307d7ffb32db9591c2df0552df23a
Remus Selea <remus.selea@gmail.com>
2021-09-26 16:44:47 +0300

    Move SearchController methods to PerfumeController - fix issue with wrong page total size being returned for perfumes when filtering results

--- src/main/java/com/github/remusselea/scentdb/controller/PerfumeController.java
+++ src/main/java/com/github/remusselea/scentdb/controller/PerfumeController.java
@@ -3,13 +3,22 @@ package com.github.remusselea.scentdb.controller;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.remusselea.scentdb.dto.Filter;
+import com.github.remusselea.scentdb.dto.model.perfume.PerfumeDto;
 import com.github.remusselea.scentdb.dto.request.PerfumeModel;
 import com.github.remusselea.scentdb.dto.request.PerfumeRequest;
 import com.github.remusselea.scentdb.dto.response.PerfumeResponse;
 import com.github.remusselea.scentdb.dto.view.View.PerfumeView;
 import com.github.remusselea.scentdb.service.PerfumeService;
+import com.github.remusselea.scentdb.service.PerfumeSearchService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,8 +36,12 @@ public class PerfumeController {
 
   private final PerfumeService perfumeService;
 
-  public PerfumeController(PerfumeService perfumeService) {
+  private final PerfumeSearchService perfumeSearchService;
+
+  public PerfumeController(PerfumeService perfumeService,
+      PerfumeSearchService perfumeSearchService) {
     this.perfumeService = perfumeService;
+    this.perfumeSearchService = perfumeSearchService;
   }
 
   /**
@@ -97,6 +110,67 @@ public class PerfumeController {
 
     perfumeService.removePerfumeById(perfumeId);
   }
+
+  @GetMapping("/perfumes/search")
+  @JsonView(value = PerfumeView.class)
+  public Page<PerfumeDto> searchPerfumes(@RequestParam(value = "q", required = false) String query,
+      @RequestParam(value = "genderFilter", required = false) String genderFilter,
+      @RequestParam(value = "yearFilter", required = false) String yearFilter,
+      @RequestParam(value = "perfumeTypeFilter", required = false) String perfumeTypeFilter,
+      @RequestParam(value = "companyFilter", required = false) String companyFilter,
+      @PageableDefault(size = 9) Pageable pageable) {
+    log.info("searching perfumes");
+
+    List<Filter> filterList = map(genderFilter);
+    List<Filter> yearFilterList = map(yearFilter);
+    List<Filter> perfumeTypeFilterList = map(perfumeTypeFilter);
+    List<Filter> companyFilterList = map(companyFilter);
+
+    return perfumeSearchService.search(pageable, query, filterList, yearFilterList, perfumeTypeFilterList, companyFilterList);
+  }
+
+  private List<Filter> map(String filters) {
+    List<Filter> filterList = new ArrayList<>();
+
+    if (filters == null) {
+      return filterList;
+    }
+
+    if (filters.contains(";")) {
+      for (String filter : filters.split(";")) {
+        filterList.addAll(this.mapValues(filter));
+      }
+    } else {
+      filterList.addAll(this.mapValues(filters));
+    }
+    return filterList.stream()
+        .filter(Objects::nonNull)
+        .collect(Collectors.toList());
+  }
+
+  private List<Filter> mapValues(String filter) {
+    List<Filter> filters = new ArrayList<>();
+    // check if filter string is valid
+    if (!filter.contains(":")) {
+      return filters;
+    }
+
+    if (filter.startsWith(",")) {
+      filter = filter.replaceFirst(",", "");
+    }
+
+    final String[] values = filter.split(":");
+    if (values[1].contains(",")) {
+      for (String f : values[1].split(",")) {
+        filters.add(new Filter(values[0], f));
+      }
+    } else {
+      filters.add(new Filter(values[0], values[1]));
+    }
+
+    return filters;
+  }
+
 
   private PerfumeRequest deserializeStringToPerfumeRequest(String perfume) {
     PerfumeRequest perfumeRequest = null;
--- src/main/java/com/github/remusselea/scentdb/service/PerfumeSearchService.java
+++ src/main/java/com/github/remusselea/scentdb/service/PerfumeSearchService.java
@@ -2,23 +2,16 @@ package com.github.remusselea.scentdb.service;
 
 
 import com.github.remusselea.scentdb.dto.Filter;
-import com.github.remusselea.scentdb.dto.mapper.NoteMapper;
 import com.github.remusselea.scentdb.dto.mapper.PerfumeMapper;
-import com.github.remusselea.scentdb.dto.model.note.NoteDto;
 import com.github.remusselea.scentdb.dto.model.perfume.Gender;
 import com.github.remusselea.scentdb.dto.model.perfume.PerfumeDto;
 import com.github.remusselea.scentdb.dto.model.perfume.Type;
-import com.github.remusselea.scentdb.dto.response.PerfumeResponse;
-import com.github.remusselea.scentdb.model.entity.Note;
 import com.github.remusselea.scentdb.model.entity.Perfume;
-import com.github.remusselea.scentdb.model.entity.PerfumeNote;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
+import javax.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.hibernate.search.backend.elasticsearch.ElasticsearchExtension;
@@ -38,7 +31,6 @@ import org.hibernate.search.mapper.orm.session.SearchSession;
 import org.hibernate.search.util.common.data.RangeBoundInclusion;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Service;
@@ -46,30 +38,31 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
-public class SearchService {
+public class PerfumeSearchService {
 
   private final EntityManager entityManager;
 
   private final PerfumeMapper perfumeMapper;
 
-  private final NoteMapper noteMapper;
-
-  public SearchService(EntityManager entityManager,
-      PerfumeMapper perfumeMapper, NoteMapper noteMapper) {
+  public PerfumeSearchService(EntityManager entityManager,
+      PerfumeMapper perfumeMapper) {
     this.entityManager = entityManager;
     this.perfumeMapper = perfumeMapper;
-    this.noteMapper = noteMapper;
   }
 
   @Transactional(readOnly = true)
-  public Page<PerfumeResponse> search(Pageable pageable, String query, List<Filter> genderFilterList,
-      List<Filter> yearFilterList, List<Filter> perfumeTypeFilterList, List<Filter> companyFilterList) {
-    Page<PerfumeResponse> perfumeResponsePage;
+  public Page<PerfumeDto> search(Pageable pageable, String query, List<Filter> genderFilterList,
+      List<Filter> yearFilterList, List<Filter> perfumeTypeFilterList,
+      List<Filter> companyFilterList) {
+    Page<PerfumeDto> perfumeResponsePage;
 
-    if ((query == null || query.isBlank()) && genderFilterList.isEmpty() && companyFilterList.isEmpty() && perfumeTypeFilterList.isEmpty() && yearFilterList.isEmpty()) {
+    if ((query == null || query.isBlank()) && genderFilterList.isEmpty()
+        && companyFilterList.isEmpty() && perfumeTypeFilterList.isEmpty()
+        && yearFilterList.isEmpty()) {
       perfumeResponsePage = searchMatchAll(pageable, query);
     } else {
-      perfumeResponsePage = searchTerms(pageable, query, genderFilterList, yearFilterList, perfumeTypeFilterList, companyFilterList);
+      perfumeResponsePage = searchTerms(pageable, query, genderFilterList, yearFilterList,
+          perfumeTypeFilterList, companyFilterList);
     }
 
     return perfumeResponsePage;
@@ -77,8 +70,9 @@ public class SearchService {
 
 
   @Transactional(readOnly = true)
-  public Page<PerfumeResponse> searchTerms(Pageable pageable, String query,
-      List<Filter> genderFilter, List<Filter> yearFilterList, List<Filter> perfumeTypeFilterList, List<Filter> companyFilterList) {
+  public Page<PerfumeDto> searchTerms(Pageable pageable, String query,
+      List<Filter> genderFilter, List<Filter> yearFilterList, List<Filter> perfumeTypeFilterList,
+      List<Filter> companyFilterList) {
 
     SearchSession searchSession = Search.session(entityManager);
     SearchScope<Perfume> scope = searchSession.scope(Perfume.class);
@@ -93,38 +87,31 @@ public class SearchService {
         .sort(searchSort)
         .toQuery();
 
-    SearchResult<Perfume> result = searchQuery.fetch((int) pageable.getOffset(), pageable.getPageSize());
-    List<Perfume> perfumeList = result.hits();
+    SearchResult<Perfume> result = searchQuery
+        .fetch((int) pageable.getOffset(), pageable.getPageSize());
 
-    PerfumeResponse perfumeResponse = createPerfumeResponse();
-    perfumeList.forEach(perfume -> addInfoToPerfumeResponse(perfumeResponse, perfume));
-
-    // create customPageable to fix the issue with total page results,
-    // caused by spring calculating the page total from the size of the list instead of just using the result total hit count
-    int pageNumber = pageable.getPageNumber() == 0 ? pageable.getPageNumber() : pageable.getPageNumber() -1;
-    Pageable customPageable = PageRequest.of(pageNumber, pageable.getPageSize(), pageable.getSort());
-
-    return new PageImpl<>(Collections.singletonList(perfumeResponse), customPageable, result.total().hitCount());
+    return getPerfumeDtoPage(pageable, result);
   }
 
   private SearchSort getSearchSort(Pageable pageable, SearchScope<Perfume> scope) {
     List<Order> sortOrderList = pageable.getSort().stream().collect(Collectors.toList());
     SearchSortFactory searchSortFactory = scope.sort();
-    CompositeSortComponentsStep<?> compositeSortComponentsStep  = searchSortFactory.composite();
+    CompositeSortComponentsStep<?> compositeSortComponentsStep = searchSortFactory.composite();
 
-    for (Order sortOrder: sortOrderList) {
+    for (Order sortOrder : sortOrderList) {
       String fieldToSortBy = sortOrder.getProperty();
 
-      if(fieldToSortBy.equals("bestMatch")) {
+      if (fieldToSortBy.equals("bestMatch")) {
         compositeSortComponentsStep.add(searchSortFactory.score());
-      } else{
+      } else {
         SortOrder order = SortOrder.DESC;
 
-        if(sortOrder.isAscending()){
+        if (sortOrder.isAscending()) {
           order = SortOrder.ASC;
         }
 
-        FieldSortOptionsStep<?, ? extends SearchPredicateFactory> fieldSortOptionsStep = searchSortFactory.field(fieldToSortBy).order(order);
+        FieldSortOptionsStep<?, ? extends SearchPredicateFactory> fieldSortOptionsStep = searchSortFactory.field(
+            fieldToSortBy).order(order);
         compositeSortComponentsStep.add(fieldSortOptionsStep.toSort());
       }
 
@@ -134,7 +121,7 @@ public class SearchService {
   }
 
   @Transactional(readOnly = true)
-  public Page<PerfumeResponse> searchMatchAll(Pageable pageable, String query) {
+  public Page<PerfumeDto> searchMatchAll(Pageable pageable, String query) {
     SearchSession searchSession = Search.session(entityManager);
     SearchScope<Perfume> scope = searchSession.scope(Perfume.class);
     SearchSort searchSort = getSearchSort(pageable, scope);
@@ -147,16 +134,26 @@ public class SearchService {
 
     SearchResult<Perfume> result = searchQuery
         .fetch((int) pageable.getOffset(), pageable.getPageSize());
+
+    return getPerfumeDtoPage(pageable, result);
+  }
+
+  @NotNull
+  private Page<PerfumeDto> getPerfumeDtoPage(Pageable pageable, SearchResult<Perfume> result) {
     List<Perfume> perfumeList = result.hits();
+    List<PerfumeDto> perfumeDtoList = new ArrayList<>(perfumeList.size());
 
-    PerfumeResponse perfumeResponse = createPerfumeResponse();
-    perfumeList.forEach(perfume -> addInfoToPerfumeResponse(perfumeResponse, perfume));
+    for (Perfume perfume : perfumeList) {
+      PerfumeDto perfumeDto = perfumeMapper.perfumeToPerfumeDto(perfume);
+      perfumeDtoList.add(perfumeDto);
+    }
 
-    return new PageImpl<>(Collections.singletonList(perfumeResponse), pageable, result.total().hitCount());
+    return new PageImpl<>(perfumeDtoList, pageable, result.total().hitCount());
   }
 
   private SearchPredicate formSearchPredicates(String query, SearchScope<Perfume> searchScope,
-      List<Filter> genderFilter, List<Filter> yearFilterList, List<Filter> perfumeTypeFilterList, List<Filter> companyFilterList) {
+      List<Filter> genderFilter, List<Filter> yearFilterList, List<Filter> perfumeTypeFilterList,
+      List<Filter> companyFilterList) {
     SearchPredicateFactory factory = searchScope.predicate();
 
     BooleanPredicateClausesStep<?> booleanJunction = factory.bool();
@@ -217,14 +214,16 @@ public class SearchService {
     if (perfumeTypeFilterList == null || perfumeTypeFilterList.isEmpty()) {
       return;
     }
-    perfumeTypeFilterList.stream().collect(Collectors.groupingBy(Filter::getKey)).forEach((key, values) -> {
-      BooleanPredicateClausesStep<?> clausesStep = factory.bool();
-      values.forEach(
-          value -> clausesStep.should(
-              factory.match().field("perfumeType").matching(Type.valueOf((String) value.getValue()))));
+    perfumeTypeFilterList.stream().collect(Collectors.groupingBy(Filter::getKey))
+        .forEach((key, values) -> {
+          BooleanPredicateClausesStep<?> clausesStep = factory.bool();
+          values.forEach(
+              value -> clausesStep.should(
+                  factory.match().field("perfumeType")
+                      .matching(Type.valueOf((String) value.getValue()))));
 
-      booleanJunction.must(clausesStep);
-    });
+          booleanJunction.must(clausesStep);
+        });
   }
 
 
@@ -233,53 +232,16 @@ public class SearchService {
     if (companyFilterList == null || companyFilterList.isEmpty()) {
       return;
     }
-    companyFilterList.stream().collect(Collectors.groupingBy(Filter::getKey)).forEach((key, values) -> {
-      BooleanPredicateClausesStep<?> clausesStep = factory.bool();
-      values.forEach(
-          value -> clausesStep.should(
-              factory.match().field("company.companyId").matching(Long.parseLong((String) value.getValue()))));
+    companyFilterList.stream().collect(Collectors.groupingBy(Filter::getKey))
+        .forEach((key, values) -> {
+          BooleanPredicateClausesStep<?> clausesStep = factory.bool();
+          values.forEach(
+              value -> clausesStep.should(
+                  factory.match().field("company.companyId")
+                      .matching(Long.parseLong((String) value.getValue()))));
 
-      booleanJunction.must(clausesStep);
-    });
+          booleanJunction.must(clausesStep);
+        });
   }
-
-
-
-  /**
-   * Creates a {@link PerfumeResponse} and adds an empty list of {@link PerfumeDto} and empty map of
-   * {@link Map noteDtoMap} to it.
-   *
-   * @return the created PerfumeResponse.
-   */
-  private PerfumeResponse createPerfumeResponse() {
-    PerfumeResponse perfumeResponse = new PerfumeResponse();
-    // Create and set the empty perfumeDtoList to perfume response
-    List<PerfumeDto> perfumeDtoList = new ArrayList<>();
-    perfumeResponse.setPerfumeDtoList(perfumeDtoList);
-    // Create and set the empty noteDtoMap to perfume response
-    Map<Long, NoteDto> noteDtoMap = new HashMap<>();
-    perfumeResponse.setNoteDtoMap(noteDtoMap);
-
-    return perfumeResponse;
-  }
-
-  /**
-   * Adds the data of a {@link Perfume} to a {@link PerfumeResponse}.
-   */
-  private void addInfoToPerfumeResponse(PerfumeResponse perfumeResponse, Perfume perfume) {
-    // map perfume to perfumeDto
-    PerfumeDto perfumeDto = perfumeMapper.perfumeToPerfumeDto(perfume);
-
-    // add each mapped perfumeDto into the perfumeDtoList of perfumeResponse
-    perfumeResponse.getPerfumeDtoList().add(perfumeDto);
-
-    // map each PerfumeNote to a NoteDto and put it in the noteDtoMap
-    for (PerfumeNote perfumeNote : perfume.getPerfumeNotes()) {
-      NoteDto noteDto = noteMapper.perfumeNoteToNoteDto(perfumeNote);
-      Note note = perfumeNote.getNote();
-      perfumeResponse.getNoteDtoMap().put(note.getNoteId(), noteDto);
-    }
-  }
-
 
 }
