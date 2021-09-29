580ba08f7a43201f14186f29e4628984f0a4c33a
Remus Selea <remus.selea@gmail.com>
2021-09-29 21:18:14 +0300

    Disable database initialization from data-postgres.sql

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,3 +1,6 @@
+-- Replace hardcoded database, user and password with value from environment variables
+-- https://github.com/remus-selea/scentdb/issues/22
+
 CREATE USER scentdb WITH ENCRYPTED PASSWORD 'scentdb';
 CREATE DATABASE scentdb;
 GRANT ALL PRIVILEGES ON DATABASE scentdb TO scentdb;
